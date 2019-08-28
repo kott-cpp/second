a9156d9413378c7517e1717b41b62084d53ba946
Kevin Ott <kott@cppwind.com>
2019-08-28 13:31:40 -0600

    router added

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import styles from '../button.module.css';
+
+const Navigation = () => {
+  return (
+    <div>
+      <Link to="/" className={styles.button}>Home</Link>
+      <Link to="/about" className={styles.button}>About</Link>
+      <Link to="/contact" className={styles.button}>Contact</Link>
+    </div>
+  );
+};
+
+export default Navigation;
