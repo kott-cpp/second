732c65a1db0f257c7c285c5427d9f35b5a5d5c6e
Kevin Ott <kott@cppwind.com>
2019-08-29 13:47:35 -0600

    removed hygne-add

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+ // toast.configure() //(remove ToastContainer from import and div to use this)
+
 
 const Home = () => {
-  const notify = () => toast("Wow so easy!");
+  const notifyA = () => toast("This is on the Bottom-Left corner", {containerId: 'A'});
+  const notifyB = () => toast("This is on the Top-Right corner", {containerId: 'B'});
+  const notifyC = () => toast("Button was clicked!", {containerId: 'C'});
 
   return (
     <div>
@@ -12,8 +16,16 @@ const Home = () => {
         <p>Home</p>
       </div>
       <div>
-        <button onClick={notify}>Notify !</button>
-        <ToastContainer/>
+        <ToastContainer enableMultiContainer containerId={'C'} position={toast.POSITION.BOTTOM_RIGHT} />
+        <button onClick={notifyC}>What does this do?</button>
+
+      </div>
+      <div>
+        <ToastContainer enableMultiContainer containerId={'A'} position={toast.POSITION.BOTTOM_LEFT} />
+        <ToastContainer enableMultiContainer containerId={'B'} position={toast.POSITION.TOP_RIGHT} />
+
+        <button onClick={notifyA}>Notify A</button>
+        <button onClick={notifyB}>Notify B</button>
       </div>
     </div>
   );
