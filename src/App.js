0bd9c6862c11042df47712859e47c69fdef58e93
kevin ott <kott@cppwind.com>
2019-09-16 00:06:45 -0600

    new components

--- src/App.js
+++ src/App.js
@@ -1,122 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch, render } from 'react-router-dom'
 import Home from './components/Home'
+import Pilot from './components/Pilot'
 import About from './components/About'
 import Contact from './components/Contact'
 import Error from './components/Error'
 import Navigation from './components/Navigation'
 import Helmet from 'react-helmet'
-import ApolloClient from 'apollo-boost'
-import { gql } from 'apollo-boost'
 import { ApolloProvider, useQuery } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-client'
 
-const client = new ApolloClient({
-  // uri: 'https://graphiql.graphcms.com/simple/v1/swapi',
-  uri: 'http://localhost:3000/graphql',
-})
-
-client.query({
-  query: gql`
-    {
-      patchPanel(patchPanelId: 1) {
-        panelJacks {
-          nodes {
-            id
-            connectedState
-            switchPortId
-          }
-        }
-        switchPorts {
-          nodes {
-            id
-            connectedState
-            panelJackId
-          }
-        }
-      }
-    }
-  `,
-})
-
-// .then(result => console.log(result.patchPanel.panelJacks.nodes[0].id))
-/*
-const panelJackId = 298
-const switchPortId = 229
-
-const ASSIGN_PORT_TO_JACK = gql`
-  mutation($input: PortToJackAssignmentInput!) {
-    portToJackAssignment(input: $input) {
-      errors {
-        ...messageFragment
-      }
-      messages {
-        ...messageFragment
-      }
-      panelJack {
-        id
-        connectedState
-        switchPortId
-      }
-      switchPort {
-        id
-        connectedState
-        panelJackId
-      }
-    }
-  }
-  fragment messageFragment on Message {
-    title
-    body
-    category
-    autoClose
-    icon
-  }
-`
-*/
-function Jacks() {
-  const { loading, error, data } = useQuery(gql`
-      {
-          patchPanel(patchPanelId: 1) {
-              panelJacks {
-                  nodes {
-                      id
-                      connectedState
-                      switchPortId
-                  }
-              }
-              switchPorts {
-                  nodes {
-                      id
-                      connectedState
-                      panelJackId
-                  }
-              }
-          }
-      }
-  `)
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error :(</p>
-  /*
-const panelJack = data.patchPanel.panelJacks.nodes.map(id)
-console.log("panelJacks", panelJack)
-
-return data.patchPanel.switchPorts.nodes.map(({ id }) => (
-  <div>
-    <ul>
-      <li>Port: {id}</li>
-    </ul>
-  </div>
-))
-*/
-
-  return data.patchPanel.panelJacks.nodes.map(({ switchPortId }) => (
-    <div>
-      <ul>
-        <li>Jack: {switchPortId}</li>
-      </ul>
-    </div>
-  ))
-}
 
 function App() {
   return (
@@ -129,12 +22,11 @@ function App() {
           <Navigation />
           <Switch>
             <Route path="/" component={Home} exact />
+            <Route path="/pilot" component={Pilot} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route component={Error} />
           </Switch>
-          <Jacks />
-          {/*<ASSIGN_PORT_TO_JACK />*/}
         </div>
       </BrowserRouter>
     </ApolloProvider>
