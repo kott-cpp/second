98d4f7c84831fd1fda627f41403320441162b58d
Kevin Ott <kott@cppwind.com>
2019-10-05 16:04:42 -0600

    failed attempt on fizzbuzz

---
+++ src/utils/failedFizzBuzz.js
@@ -0,0 +1,21 @@
+function failedFizzBuzz(num) {
+  const input = JSON.stringify(num)
+  const fbNumbers = ['5', '7']
+  let resultArray = []
+
+  for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+    for (let fbNumber = 0; fbNumber < fbNumbers.length; fbNumber++) {
+      if (input[inputIndex] === fbNumbers[fbNumber]) {
+        if (input[inputIndex] === '5') {
+          resultArray.push('fizz')
+        } else if (input[inputIndex] === '7') {
+          resultArray.push('buzz')
+        }
+      }
+    }
+  }
+  console.log(resultArray.join(''))
+}
+
+failedFizzBuzz(57)
+module.exports = failedFizzBuzz
--- src/utils/failedFizzBuzz.test.js
+++ src/utils/failedFizzBuzz.test.js
@@ -1,5 +1,5 @@
-const fizzBuzz = require('./fizzBuzz');
-const input = require('./fizzBuzz')
+const fizzBuzz = require('./failedFizzBuzz');
+
 
 // https://github.com/jest-community/jest-extended
 
@@ -8,5 +8,3 @@ const input = require('./fizzBuzz')
 //https://jestjs.io/docs/en/expect
 
 it('fizzBuzz is a function', () => expect(fizzBuzz).toBeFunction)
-
-it('input is not a string', () => expect(input).toBeNumber())
