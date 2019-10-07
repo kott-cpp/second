cb916e6eabdb46603b81a1aea1b57889191e4c64
Kevin Ott <kott@cppwind.com>
2019-10-07 14:52:27 -0600

    some twittle work

--- src/components/Twittle/index.js
+++ src/components/Twittle/index.js
@@ -3,32 +3,28 @@ import styles from './style.css'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faAngleUp } from '@fortawesome/pro-solid-svg-icons'
+import { faAngleDown, faAngleUp, faCheck } from '@fortawesome/pro-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 const setupIcons = () => {
-  library.add(faAngleDown, faAngleUp)
+  library.add(faAngleDown, faAngleUp, faCheck)
 }
 setupIcons()
 
 const Twittle = ({ className, icon }) => {
-  const [toggled, toggle] = useState(false)
+  const [toggled, toggle] = useState(false);
   return (
     <React.Fragment>
-      <div className={'boundary'}>
-        <article>
-          <header>
-            <div className={cx(className, 'headerLeft')}>Title of page</div>
-            <span
-              className={cx(className, 'headerRight', 'angle-down')}
-              onClick={() => toggled => !toggled}
-            >
-
-            </span>
-          </header>
-          <section>{toggled && <React.Fragment>hello</React.Fragment>}</section>
-        </article>
-      </div>
+      <article>
+        <header>
+          <div className={cx(className, 'headerLeft')}>Title of page</div>
+          <span
+            className={cx(className, 'headerRight fa-rotate-180')}
+            onClick={() => toggle(toggled => !toggled)}
+          > {icon && <Icon icon={icon} />} </span>
+        </header>
+        {toggled && <><section className={cx('transition')}>hello</section></>}
+      </article>
     </React.Fragment>
   )
 }
