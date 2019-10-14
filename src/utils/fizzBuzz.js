e650accb8ad50857f22016221f74c2f318ff1c1d
kevin ott <kott@cppwind.com>
2019-10-14 07:56:56 -0600

    some changes

--- src/utils/fizzBuzz.js
+++ src/utils/fizzBuzz.js
@@ -1,18 +1,15 @@
 let arr = []
 function fizzBuzz(num) {
-  for (let i = num; i <= num; i++) {
-    if ((num + '').includes('7') && (num + '').includes('5')) {
-      return 'fizzBuzz'
-    } else if ((num + '').includes('5')) {
-      return 'fizz'
-    } else if ((num + '').includes('7')) {
-      return 'buzz'
-    } else {
-      return ''
-    }
+
+  if ((num + '').includes('7') && (num + '').includes('5')) {
+    return 'fizzBuzz'
+  } else if ((num + '').includes('5')) {
+    return 'fizz'
+  } else if ((num + '').includes('7')) {
+    return 'buzz'
+  } else {
+    return ''
   }
-  arr.push(num)
-  console.log(arr)
 }
 
 module.exports = fizzBuzz
