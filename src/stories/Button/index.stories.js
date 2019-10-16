707edd64c8d25d8d892eff717cb938fb464202a3
kevin ott <kott@cppwind.com>
2019-10-16 13:01:15 -0600

    added knobs

--- src/stories/Button/index.stories.js
+++ src/stories/Button/index.stories.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import ButtonStory from './Button'
-import {storiesOf} from '@storybook/react'
+import { storiesOf } from '@storybook/react'
+import { text } from '@storybook/addon-knobs'
 
 const buttonStory = storiesOf('Button')
+
+/*
+buttonStory.addDecorator(withKnobs)
+buttonStory.add('Default', () => {
+  const label = text('Label', 'Button')
+
+  const doClick = action('Button Click')
+  return (
+    <section>
+      <Button label={label} />
+    </section>
+  )
+})
+*/
+ 
 
 buttonStory.add('Default', () => <ButtonStory />)
