e3392a62a6bd749bf7b6aeb76c1ca90b7e2502b5
kevin ott <kott@cppwind.com>
2020-01-22 18:21:44 -0700

    added

---
+++ src/components/GriddleTable.js
@@ -0,0 +1,56 @@
+import cx from 'classnames'
+import React from 'react'
+import styles from './styles.module.css'
+import Griddle, { plugins } from 'griddle-react'
+
+export const data = [
+  {
+    id: 0,
+    name: 'Mayer Leonard',
+    city: 'Kapowsin',
+    state: 'Hawaii',
+    country: 'United States',
+    company: 'Ovolo',
+    favoriteNumber: 7,
+  },
+  {
+    id: 1,
+    name: 'Koch Becker',
+    city: 'Johnsonbrg',
+    state: 'New Jersey',
+    country: 'Madagascar',
+    company: 'Eventage',
+    favoriteNumber: 2,
+  },
+  {
+    id: 2,
+    name: 'Lowery Hopkins',
+    city: 'Blanco',
+    state: 'Arizona',
+    country: 'Ukraine',
+    company: 'Comtex',
+    favoriteNumber: 3,
+  },
+  {
+    id: 3,
+    name: 'Walters Mays',
+    city: 'Glendale',
+    state: 'Illinois',
+    country: 'New Zealand',
+    company: 'Corporana',
+    favoriteNumber: 6,
+  },
+  {
+    id: 4,
+    name: 'Shaw Lowe',
+    city: 'Coulterville',
+    state: 'Wyoming',
+    country: 'Equadaor',
+    company: 'Isologica',
+    favoriteNumber: 2,
+  },
+]
+
+export const GriddleTable = () => {
+  return <Griddle data={data} plugins={[plugins.LocalPlugin]} />
+}
